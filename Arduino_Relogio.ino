74841d9daa3bd977022e698d2cb53d4d91d7c4c5
Tamashi19 <29raul19@gmail.com>
2021-10-23 13:06:37 -0300

    teste

--- Arduino_Relogio.ino
+++ Arduino_Relogio.ino
@@ -5,10 +5,49 @@ LedControl led(A3,A5,A4,1);
 const byte CI = 0, BT_SET = 2, BT_UP = 3, BT_DW =4, SPK =5;
 byte sec =  0;
 
+void alarme(){
+  if(!(sec%2)) tone(SPK, 180,180);
+}
+
+void Display(int h,int m ,int mod){
+
+    static int blk = 0;
+    if (mod<4){
+      if(++blk >7) blk=0;
+    }else{
+      blk=0;
+    }
+    
+    if(h< 0) h += 10 +(mod ==0) *20;
+    if(m<0) m +=10 + (mod ==2) *50;
+    if(sec > 59){
+      sec=0;
+      m+=1;
+    }
+    if(m > 59){
+      m=0;
+      h+=1;
+    }
+    if(h>23) h =0;
+    
+    led.setDigit(CI,0,h,0);
+    led.setDigit(CI,1,h%10,0);
+    led.setDigit(CI,2,m/10,0);
+    led.setDigit(CI,3,m%10,0);
+    led.setDigit(CI,4,sec/10,0);
+    led.setDigit(CI,5,sec%10,0);
+
+    if(blk>3) led.setChar(CI,mod, ' ',0);
+}
+
+
+void clk(){
+  sec++;
+}
 void setup() {
-  led.shutdown(CI, false)
-  led,setIntensity(CI,8);
-  for(byte i = 2; i < 5; i++) pinMode(I, INPUT_PULLUP);
+  led.shutdown(CI, false);
+  led.setIntensity(CI,8);
+  for(byte i = 2; i < 5; i++) pinMode(i, INPUT_PULLUP);
   pinMode(SPK,OUTPUT);
   Timer1.initialize(1000000); //1 segundo
   Timer1.attachInterrupt(clk);
@@ -20,7 +59,7 @@ void loop() {
 }
 
 void ajust(){
-  int h,m,al_h, al_m, ax_h =0, ax_m=0, mod=3:
+  int h,m,al_h, al_m, ax_h =0, ax_m=0, mod=3;
   bool pres = false, alarm = false;
   while(1){
     
@@ -61,7 +100,7 @@ void ajust(){
       if(mod-- ==0){
         alarm = !alarm;
         if (alarm) mod = 3;
-        else mod = 4
+        else mod = 4;
       }
     }
     pres = true;
@@ -71,43 +110,5 @@ void ajust(){
     if(al_m==m and al_h == h and (al_m or al_h) and mod==4) alarme();
   }
   
-}
-
-void Display(int *h,int *m ,int mod){
-
-    static int blk = 0;
-    if (mod<4){
-      if(++blk >7) blk=0;
-    }else{
-      blk=0;
-    }
-    
-    if(*h< 0) *h += 10 +(mod ==0) *20;
-    if(*m<0) *m +=10 + (mod ==2) *50;
-    if(sec > 59){
-      sec=0;
-      *m+=1;
-    }
-    if(*m > 59){
-      *m=0;
-      *h+=1;
-    }
-    if(*h>23) *h =0;
-    
-    led.setDigit(CI,0,*h,0);
-    led.setDigit(CI,1,*h%10,0);
-    led.setDigit(CI,2,*m/10,0);
-    led.setDigit(CI,3,*m%10,0);
-    led.setDigit(CI,4,*sec/10,0);
-    led.setDigit(CI,5,*sec%10,0);
-
-    if(blk>3) led.setChar(CI,mod, ' ',0);
-}
-
-void clk(){
-  sec++;
-}
-
-void alarmE(){
-  if(!(sec%2)) tone(SPK, 180,180);
+  }
 }
